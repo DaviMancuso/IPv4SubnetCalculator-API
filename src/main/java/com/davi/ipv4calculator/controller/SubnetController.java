5bdde776e323f68dc874a3f5d3b3230fb4c77f34
E-MAIL PRINCIPAL <mancusodavidelimamancuso@gmail.com>
2026-06-17 15:59:17 -0400

    feat: adicionar Swagger e Sistema de Logs

--- src/main/java/com/davi/ipv4calculator/controller/SubnetController.java
+++ src/main/java/com/davi/ipv4calculator/controller/SubnetController.java
@@ -2,6 +2,8 @@ package com.davi.ipv4calculator.controller;
 
 import com.davi.ipv4calculator.model.SubnetInfo;
 import com.davi.ipv4calculator.service.SubnetCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,14 +11,22 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class SubnetController {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(SubnetController.class);
+
     private final SubnetCalculator calculator;
 
-    public SubnetController(SubnetCalculator calculator) {
+    public SubnetController(SubnetCalculator calculator, SubnetCalculator subnetCalculator) {
         this.calculator = calculator;
     }
 
     @GetMapping
     public SubnetInfo calcular(@RequestParam String ip) {
+        logger.info("Requisição recebida pelo cáculo de sub-rede: {}", ip);
+
+        SubnetInfo resultado = calculator.calcular(ip);
+        logger.info("Cálculo realizado com sucesso para {}", ip);
+
         return calculator.calcular(ip);
     }
 }
--- src/main/java/com/davi/ipv4calculator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/davi/ipv4calculator/exception/GlobalExceptionHandler.java
@@ -6,16 +6,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String, String>> handleIllegalArgument(
             IllegalArgumentException e
+
     ) {
+        logger.warn("Erro de validação na requisição: {}", e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("erro", e.getMessage()));
